5052c2d3a809cf779d2203f23f1ce6bbd4411422
VSeiji <rm93380@fiap.com.br>
2023-09-19 09:04:40 -0300

    Testes

--- src/app/alimentacao/page.js
+++ src/app/alimentacao/page.js
@@ -11,7 +11,7 @@ export default async function Alimentacao() {
         <main className="bg-amber-50 mt-10 p-12 rounded max-w-lg m-auto">
           <h2 className="text-xl italic text-center"> Sua Alimentação</h2>
           <div id="data">
-            {(refeicao).map((refeicao) => <DataRow key={refeicao.user.id} refeicao={refeicao}/>)}
+            
           </div>
         </main>
       </>
