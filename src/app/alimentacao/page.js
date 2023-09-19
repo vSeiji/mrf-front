5d0c1bd07786306a4d8494e6e1232ee5b82323c2
VSeiji <rm93380@fiap.com.br>
2023-09-19 08:52:14 -0300

    Mudanças

--- src/app/alimentacao/page.js
+++ src/app/alimentacao/page.js
@@ -2,16 +2,16 @@ import NavBar from "@/components/NavBar";
 import DataRow from "./DataRow";
 import { getRefeicao } from "@/actions/contas";
 
-
 export default async function Alimentacao() {
-  const refeicao = await getRefeicao()
+  let refeicao = await getRefeicao()
+  refeicao = refeicao._embedded.entityModelList
     return (
       <>
         <NavBar active={"alimentacao"}/>
         <main className="bg-amber-50 mt-10 p-12 rounded max-w-lg m-auto">
           <h2 className="text-xl italic text-center"> Sua Alimentação</h2>
           <div id="data">
-            {refeicao.map(refeicao => <DataRow key={refeicao.user.id} refeicao={refeicao}/>)}
+            {(refeicao).map((refeicao) => <DataRow key={refeicao.user.id} refeicao={refeicao}/>)}
           </div>
         </main>
       </>
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button() {
+    return(
+        <Link href="#">Botão</Link>
+    )
+}
